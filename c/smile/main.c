4c15504631f16e573d78a4bf5fd086ccc337cd25
donilan <donilan.qq@gmail.com>
2012-11-16 17:34:09 +0800

    add some code

---
+++ c/smile/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h> 
+
+int main(void) 
+{ 
+  const short int c1 = 49920;
+  const int c2 = 1073742008;
+
+  int (*pf)() = (int (*)())&c2;
+  printf("%c%c\n", *(char *)pf()-19, *((char*)pf()+1)-49);
+  return 0;
+}
+
