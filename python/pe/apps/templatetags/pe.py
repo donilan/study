147e5e8569ceae7a314e532543fddb4d081bc479
donilan <donilan.qq@gmail.com>
2012-12-01 20:22:11 +0800

    Add some code.

--- python/pe/apps/templatetags/pe.py
+++ python/pe/apps/templatetags/pe.py
@@ -26,6 +26,24 @@ _SECTION_FLAGS = {
     0x80000000: "<span class='important' >Section is writable.</span>"
 }
 
+_CHARACTERISTICS_FLAGS = {
+    0x0001:  'IMAGE_FILE_RELOCS_STRIPPED Relocation info stripped from file.',
+    0x0002:  'IMAGE_FILE_EXECUTABLE_IMAGE File is executable  (i.e. no unresolved externel references).',
+    0x0004:  'IMAGE_FILE_LINE_NUMS_STRIPPED Line nunbers stripped from file.',
+    0x0008:  'IMAGE_FILE_LOCAL_SYMS_STRIPPED Local symbols stripped from file.',
+    0x0010:  'IMAGE_FILE_AGGRESIVE_WS_TRIM Agressively trim working set',
+    0x0020:  'IMAGE_FILE_LARGE_ADDRESS_AWARE App can handle >2gb addresses',
+    0x0080:  'IMAGE_FILE_BYTES_REVERSED_LO Bytes of machine word are reversed.',
+    0x0100:  'IMAGE_FILE_32BIT_MACHINE 32 bit word machine.',
+    0x0200:  'IMAGE_FILE_DEBUG_STRIPPED Debugging info stripped from file in .DBG file',
+    0x0400:  'IMAGE_FILE_REMOVABLE_RUN_FROM_SWAP If Image is on removable media, copy and run from the swap file.',
+    0x0800:  'IMAGE_FILE_NET_RUN_FROM_SWAP If Image is on Net, copy and run from the swap file.',
+    0x1000:  'IMAGE_FILE_SYSTEM System File.',
+    0x2000:  'IMAGE_FILE_DLL File is a DLL.',
+    0x4000:  'IMAGE_FILE_UP_SYSTEM_ONLY File should only be run on a UP machine',
+    0x8000:  'IMAGE_FILE_BYTES_REVERSED_HI Bytes of machine word are reversed.',
+}
+
 def _splitReverse(strs):
     return ''.join([strs[:i][::-1] for i in range(0,len(strs)+4,4)])
         
@@ -60,6 +78,13 @@ def sectionFlags(value):
             result.append(_SECTION_FLAGS[k])
     return '<br />'.join(result)
 
+def characteristicsFlag(value):
+    result = []
+    for k in _CHARACTERISTICS_FLAGS.keys():
+        if (k & value) > 0:
+            result.append(_CHARACTERISTICS_FLAGS[k])
+    return '<br />'.join(result)
+
 
 _MACHINES = {
     '014C': 'Intel 80386',
@@ -81,7 +106,18 @@ def machine(value):
     """
     return _MACHINES.get(str2hex(value),'Unknow machine')
 
+def dataListLength(value):
+    """
+    Calc length of data list.
+    Arguments:
+    - `value`:
+    """
+    if isinstance(value, list):
+        return sum(d.structLength for d in value)
+
 register.filter('hexFormat', hexFormat)
 register.filter('sectionFlags', sectionFlags)
 register.filter('machine', machine)
 register.filter('hex', str2hex)
+register.filter('dataListLength', dataListLength)
+register.filter('characteristicsFlag', characteristicsFlag)
