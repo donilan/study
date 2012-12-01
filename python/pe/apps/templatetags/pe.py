ba6b401b3af14ead86c93e320bfcb0082fd62263
donilan <donilan.qq@gmail.com>
2012-12-01 18:25:05 +0800

    Fix a bug.

--- python/pe/apps/templatetags/pe.py
+++ python/pe/apps/templatetags/pe.py
@@ -4,26 +4,26 @@ from django import template
 register = template.Library()
 
 _SECTION_FLAGS = {
-    8: "Section should not be padded to next boundary.",
-    20: "<span class='important' >Section contains code.</span>",
-    40: "Section contains initialised data (which will become initialised with real values before the file is launched).",
-    80: "Section contains uninitialised data (which will be initialised as 00 byte values before launch).",
-    200: "Section contains comments for the linker.",
-    800: "Section contents will not become part of image.",
-    1000: "Section contents comdat (Common Block Data).",
-    8000: "Section contents cannot be accessed relative to GP.",
-    100000: "Boundary alignment settings.",
-    200000: "Boundary alignment settings.",
-    400000: "Boundary alignment settings.",
-    800000: "Boundary alignment settings.",
-    1000000: "Section contains extended relocations.",
-    2000000: "Section can be discarded (e.g. .reloc).",
-    4000000: "Section is not cacheable.",
-    8000000: "Section is pageable.",
-    10000000: "Section is shareable.",
-    20000000: "<span class='important' >Section is executable.</span>",
-    40000000: "<span class='important' >Section is readable.</span>",
-    80000000: "<span class='important' >Section is writable.</span>"
+    0x00000008: "Section should not be padded to next boundary.",
+    0x00000020: "<span class='important' >Section contains code.</span>",
+    0x00000040: "Section contains initialised data (which will become initialised with real values before the file is launched).",
+    0x00000080: "Section contains uninitialised data (which will be initialised as 00 byte values before launch).",
+    0x00000200: "Section contains comments for the linker.",
+    0x00000800: "Section contents will not become part of image.",
+    0x00001000: "Section contents comdat (Common Block Data).",
+    0x00008000: "Section contents cannot be accessed relative to GP.",
+    0x00100000: "Boundary alignment settings.",
+    0x00200000: "Boundary alignment settings.",
+    0x00400000: "Boundary alignment settings.",
+    0x00800000: "Boundary alignment settings.",
+    0x01000000: "Section contains extended relocations.",
+    0x02000000: "Section can be discarded (e.g. .reloc).",
+    0x04000000: "Section is not cacheable.",
+    0x08000000: "Section is pageable.",
+    0x10000000: "Section is shareable.",
+    0x20000000: "<span class='important' >Section is executable.</span>",
+    0x40000000: "<span class='important' >Section is readable.</span>",
+    0x80000000: "<span class='important' >Section is writable.</span>"
 }
 
 def _splitReverse(strs):
